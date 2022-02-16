4b91c045017391503ad7c0aa47fab241ce073f45
tariktesfa <tariktesfa9090@gmail.com>
2022-02-16 16:54:18 +0300

    Refactor class by hook

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import Calculate from '../logic/Calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { currentValue: 0, operator: '', nextValue: '' };
-  }
+const Calculator = () => {
+  const [data, setData] = useState({
+    currentValue: 0,
+    operator: '',
+    nextValue: '',
+  });
 
-  HandleClick = (items) => this.setState((e) => Calculate(e, items.target.textContent));
+  const handleClick = (e) => {
+    const result = Calculate(data, e.target.textContent);
+    setData(result);
+  };
 
-  render() {
-    const { currentValue, operator, nextValue } = this.state;
-    const display = `${currentValue || operator || nextValue ? `${currentValue || ''} ${operator || ''} ${nextValue || ''}` : '0'} `;
-    const keyArray = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
-      .map((keys) => <button type="button" className="button" key={keys} id={keys} onClick={this.HandleClick}>{keys}</button>);
+  const display = `${data.currentValue || data.operator || data.nextValue ? `${data.currentValue || ''} ${data.operator || ''} ${data.nextValue || ''}` : '0'} `;
+  const keyArray = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
+    .map((keys) => <button type="button" className="button" key={keys} id={keys} onClick={handleClick}>{keys}</button>);
 
-    return (
-      <div className="calculator">
-        <div className="display">{display}</div>
-        <div className="button-keys">{keyArray}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calculator">
+      <div className="display">{display}</div>
+      <div className="button-keys">{keyArray}</div>
+    </div>
+  );
+};
 
 export default Calculator;
